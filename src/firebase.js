db87d62c493a380e3ebf5d6e7e827cc09420a952
kdw <zkdlwu94@naver.com>
2021-04-12 22:14:26 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD-xoB1GuAaGKaZ-4vfInts_FWJhVFs7HA",
+    authDomain: "yotube-clone-b5591.firebaseapp.com",
+    projectId: "yotube-clone-b5591",
+    storageBucket: "yotube-clone-b5591.appspot.com",
+    messagingSenderId: "860659293644",
+    appId: "1:860659293644:web:974ae4e958a1f4e1c21169",
+    measurementId: "G-MK9G1VRKS9"
+};
